73ba07f0552066533bccc3d5c54473ae9c7c3861
Harvin Llanes <harvin.llanes@gmail.com>
2016-09-19 16:52:23 -0600

    Update schema

--- perception_ios/Router.swift
+++ perception_ios/Router.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import Alamofire
+
+enum TodoRouter:  URLRequestConvertible {
+    
+    static let baseURL = URL(string: "http://localhost:8100/api")
+    
+    case Items
+    
+    
+    var route : (path: String, parameters: [String: AnyObject]?) {
+        switch self {
+        case .Items:
+            return ("/items/",nil)
+        }
+    }
+    var url : URL! { return TodoRouter.baseURL?.appendingPathComponent(route.path) }
+ 
+    func asURLRequest() throws -> URLRequest {
+       
+        return try! Alamofire
+            .URLEncoding()
+            .encode(URLRequest(url: url), with: route.parameters)
+        
+    }
+    
+}
