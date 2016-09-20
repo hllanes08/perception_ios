b6decd157b9d6a0007fcd2c1ba346b1ab78156ff
Harvin Llanes <harvin.llanes@gmail.com>
2016-09-20 13:41:59 -0600

    Setup action for login

--- perception_ios/User.swift
+++ perception_ios/User.swift
@@ -15,10 +15,10 @@ class User {
     private var password :String
     private var email :String
     private var auht_token :String
-    init(username:String, password: String, email: String){
+    init(username:String, password: String){
         self.username  = username
         self.password = password
-        self.email = email
+        self.email = ""
         self.auht_token = ""
     }
     func setToken(auth_token:String){
--- perception_ios/ViewController.swift
+++ perception_ios/ViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 import Alamofire
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var btLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let user = User(username: "", password: "", email: "" )
-        Alamofire.request(TodoRouter.Login(user))
-        .responseJSON { (response) in
-            debugPrint(response)
-        }
+       // let user = User(username: "", password: "", email: "" )
+       // Alamofire.request(TodoRouter.Login(user))
+       // .responseJSON { (response) in
+       //     debugPrint(response)
+      //  }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,6 +29,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeUsername(_ sender: UITextField) {
+        
+    }
 
+    @IBAction func onClickLogin(_ sender: AnyObject) {
+        var username = txtUsername.text
+        var password = txtPassword.text
+        var user = User(username,password)
+        
+    }
 }
 
