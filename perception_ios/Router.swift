05cf73e7fcffc982a99c5f67c3386574affc0497
Harvin Llanes <harvin.llanes@gmail.com>
2016-10-03 17:34:36 -0600

    Add domain exc for app hk

--- perception_ios/Router.swift
+++ perception_ios/Router.swift
@@ -11,7 +11,7 @@ import Alamofire
 
 enum TodoRouter:  URLRequestConvertible {
     
-    static let baseURL = URL(string: "http://localhost:3000//api/v1")
+    static let baseURL = URL(string: "http://pitonisa.herokuapp.com/api/v1")
     
     case Items
     case Login(User)
